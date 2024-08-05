aa70b4cf9488653a57def7e27fb2209bd7551a3b
WJ-Boshoff <willemwors15@gmail.com>
2024-08-05 18:22:21 +0200

    Member Methods Added

--- src/main/java/Methods/MemberActions.java
+++ src/main/java/Methods/MemberActions.java
@@ -12,6 +12,7 @@ import java.io.*;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MemberActions {
 
@@ -32,6 +33,30 @@ public class MemberActions {
                 }
             })
             .create();
+
+
+
+    public static void displayMembers(ArrayList<Member> members) {
+        StringBuilder output = new StringBuilder();
+        for (Member member : members) {
+            output.append("___________________________" +
+                    "\nName: ").append(member.getName()).append("\n");
+            output.append("Email: ").append(member.getEmail()).append("\n");
+            output.append("\nBorrowed Books:\n");
+            if (member.getBorrowedBooks().isEmpty()) {
+                output.append("No books borrowed.\n");
+            } else {
+                for (Book book : member.getBorrowedBooks()) {
+                    output.append("  Title: ").append(book.getTitle()).append("\n");
+                    output.append("  Author: ").append(book.getAuthor()).append("\n" +
+                            "___________________________");
+                }
+            }
+            output.append("\n");
+        }
+        System.out.println(output.toString());
+    }
+
 
     public static ArrayList<Member> loadExistingMembers(String memberFilePath){
         ArrayList<Member> existingMembers = new ArrayList<>();
@@ -73,6 +98,55 @@ public class MemberActions {
         writeMemberToFile(member, memberFilePath);
     }
 
+    public static Member LogIn(String FilePath) {
+        Member signedInMember = null;
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Member> currentMembers = loadExistingMembers(FilePath);
+
+        boolean validUsername = false;
+        while (!validUsername) {
+            System.out.println("Enter your username: ");
+            String username = scanner.nextLine();
+            for (Member member : currentMembers) {
+                if (member.name.equals(username)) {
+                    validUsername = true;
+                    System.out.println("Enter Password: ");
+                    String password = scanner.nextLine();
+                    if (member.password.equals(password)) {
+                        signedInMember = member;
+                        break;
+                    } else {
+                        System.out.println("Incorrect password.");
+                        validUsername = false;
+                    }
+                }
+            }
+            if (!validUsername) {
+                System.out.println("Username not found. Please try again.");
+            }
+        }
+        return signedInMember;
+    }
 
 
+    public static Member SignUp(Scanner scanner, String filePath) {
+        ArrayList<Member> existingMembers = loadExistingMembers(filePath);
+
+        System.out.println("Enter the Your name and surname: ");
+        String addedName = scanner.nextLine();
+
+        System.out.println("Enter the Password of the member: ");
+        String addedPassword = scanner.nextLine();
+
+        System.out.println("Enter your email : ");
+        String addedEmail = scanner.nextLine();
+
+        ArrayList<Book> borrowedBooks = new ArrayList<>();
+
+        Member newMember = new Member(addedName, addedPassword, addedEmail, borrowedBooks);
+        existingMembers.add(newMember);
+        writeMembers(existingMembers, filePath);
+        return newMember;
+    }
 }
